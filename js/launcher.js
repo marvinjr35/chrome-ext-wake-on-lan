a356b490a38827764c003fd5f4578d9189b4c767
Mike Frysinger <vapier@gentoo.org>
2019-12-26 23:19:58 +0545

    set window frame color based on theme

--- js/launcher.js
+++ js/launcher.js
@@ -2,5 +2,26 @@
 // Released into the public domain.
 
 chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create('main.html');
+	chrome.storage.local.get(['theme'], (items) => {
+		let theme = items['theme'];
+		if (theme) {
+			// Sanity check the values.
+			if (theme != 'light' && theme != 'dark') {
+				theme = undefined;
+				chrome.storage.local.remove('theme');
+			}
+		}
+
+		if (!theme) {
+			theme = window.matchMedia('(prefers-color-scheme: light)') ?
+				'light' : 'dark';
+		}
+
+		const frame = theme == 'light' ? '#eee' : '#111';
+		chrome.app.window.create('main.html', {
+			frame: {
+				color: frame,
+			},
+		});
+	});
 });
